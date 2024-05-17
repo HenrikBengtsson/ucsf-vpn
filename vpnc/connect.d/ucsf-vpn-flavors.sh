21e1a1dad6156b5659cac08525d50680e448d544
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-05-17 14:28:03 -0700

    Add ucsf_vpn_log() for hook scripts to use

--- vpnc/connect.d/ucsf-vpn-flavors.sh
+++ vpnc/connect.d/ucsf-vpn-flavors.sh
@@ -12,8 +12,15 @@
 
 ## Called via 'ucsf-vpn'?
 if [ -n "${UCSF_VPN_VERSION}" ]; then
-    file="${UCSF_VPN_FLAVOR}"/pre-connect.sh
-    if [ -f "${file}" ]; then
-        . "${file}"
+    ucsf_vpn_log() {
+       echo "[$(date --iso-8601=seconds)] $@" >> "${UCSF_VPN_LOGFILE}"
+    }
+
+    _hook_file_="${UCSF_VPN_FLAVOR}"/pre-connect.sh
+    if [ -f "${_hook_file_}" ]; then
+        ucsf_vpn_log "${_hook_file_} ..."
+        _hook_status_="done"
+        . "${_hook_file_}" || _hook_status_="error"
+        ucsf_vpn_log "${_hook_file_} ... ${_hook_status_}"
     fi
 fi
