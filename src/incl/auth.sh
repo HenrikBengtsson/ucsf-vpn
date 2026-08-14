d6a6ce2c8dbaa6515418555fbb42e13c87146c83
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-08-14 11:21:50 +0200

    Make sure leading and trailing whitespace is always trimmed when prompting user for username and password

--- src/incl/auth.sh
+++ src/incl/auth.sh
@@ -92,10 +92,10 @@ function prompt_user() {
             _tput setaf 11  ## bright yellow
             printf "Enter your UCSF Active Directory username: "
             _tput setaf 15  ## bright white
-            read -r user
+            ## IFS make sure whitespace is trimmed
+            IFS=$' \t\n' read -r user
             _tput sgr0      ## reset
         } 1>&2
-        user=${user/ /}
     done
     mdebug "- user=${user}"
 }
@@ -109,10 +109,10 @@ function prompt_pwd() {
             _tput setaf 11  ## bright yellow
             printf "Enter your UCSF Active Directory password: "
             _tput setaf 15  ## bright white
-            read -r -s pwd
+            ## IFS make sure whitespace is trimmed
+            IFS=$' \t\n' read -r -s pwd
             _tput sgr0      ## reset
         } 1>&2
-        pwd=${pwd/ /}
     done
     mecho "<password>"
 
